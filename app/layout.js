ea218c4db619d66b56aa65da46e10fce38b1dc7f
TU7SHAR <brahmin00011@gmail.com>
2024-12-01 10:39:12 +0530

    Favicon Fix:

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Stoccky",
+  title: "Stoccy",
   description: "Simple tool for Trend Analysis",
 };
 
